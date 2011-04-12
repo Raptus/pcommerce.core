81bf6fa35d1fdaea697a3d9a0cb946c06b7bb4b2
khink <email@example.com>
2011-04-12 15:22:48 +0000

    add wishlist

--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ setup(name='pcommerce.core',
       description="eCommerce for Plone",
       long_description=open("README.txt").read() + "\n" +
                        open(os.path.join("docs", "USAGE.txt")).read() + "\n" +
+                       open(os.path.join("docs", "TODO.txt")).read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
