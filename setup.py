08c8cf588df3ce755d332d219bfbb783a17f1b81
khink <email@example.com>
2011-04-12 15:18:22 +0000

    added rudimentary users' manual

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ setup(name='pcommerce.core',
       version=version,
       description="eCommerce for Plone",
       long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "USAGE.txt")).read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
