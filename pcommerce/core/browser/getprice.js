3529b84db91eb2c1ffa0b9718c9f9b3d2c9cff3a
huub_bouma <email@example.com>
2012-10-18 15:55:42 +0000

    small javascript fix to make it work with latest jquery

--- pcommerce/core/browser/getprice.js
+++ pcommerce/core/browser/getprice.js
@@ -3,24 +3,24 @@
  */
 jq('document').ready(function() {
     
-    jq('.buyViewlet select[name=cartVariation:list]').each(function(i) {
+    jq('.buyViewlet select[name="cartVariation:list"]').each(function(i) {
         jq(this).find('option').each(function(i) {
             html = jq(this).html();
             jq(this).html(html.substring(0, html.lastIndexOf('(')));
         });
     });
     
-    if (jq('.buyViewlet select[name=cartVariation:list]').length) {
+    if (jq('.buyViewlet select[name="cartVariation:list"]').length) {
       pcommerceLoadPrice();
     }
-    jq('.buyViewlet select[name=cartVariation:list]').change(function(){
+    jq('.buyViewlet select[name="cartVariation:list"]').change(function(){
         pcommerceLoadPrice();
     });
 });
 
 function pcommerceLoadPrice(){
     var variations = [];
-    jq('.buyViewlet select[name=cartVariation:list]').each(function(i) {
+    jq('.buyViewlet select[name="cartVariation:list"]').each(function(i) {
         variations[i] = jq(this).attr('value');
     });
     jq('.portletInfoBox .priceInfo').load('getprice?v='+variations.join(','));
